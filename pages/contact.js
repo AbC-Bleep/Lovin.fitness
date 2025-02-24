2f1d5c72024e567edef31cb9f7dad5da23893b88
AbC-Bleep <denilovin@gmail.com>
2025-02-24 12:13:59 -0700

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
     <div style={{ textAlign: "center", padding: "20px" }}>
       <Navbar />
       <h1>Contact Us</h1>
-      <p>Have questions? Reach out to us at <strong>support@lovinfitness.com</strong></p>
+      <p>Have questions? Reach out at <strong>dlovinfitness@gmail.com</strong></p>
     </div>
   );
 }
