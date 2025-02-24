dd83072d47e0df158aa79a30a00eea7d3ef8caf8
AbC-Bleep <denilovin@gmail.com>
2025-02-23 19:59:44 -0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Lovin Fitness</title>
+        <meta name="description" content="Personalized Workout Plans" />
+      </Head>
+      <h1>Welcome to Lovin Fitness</h1>
+      <p>Building Stronger Women - Your personalized workout journey starts here!</p>
+    </div>
+  );
+}
