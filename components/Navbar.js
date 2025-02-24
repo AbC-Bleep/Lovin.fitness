6365c17d278974b40dd1a75b2cdb071675d635d0
AbC-Bleep <denilovin@gmail.com>
2025-02-24 12:05:01 -0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,10 +3,18 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav style={{ display: "flex", gap: "20px", padding: "10px", background: "#ff4a6e", color: "white", justifyContent: "center" }}>
-      <Link href="/" style={{ textDecoration: "none", color: "white" }}>Home</Link>
-      <Link href="/about" style={{ textDecoration: "none", color: "white" }}>About</Link>
-      <Link href="/pricing" style={{ textDecoration: "none", color: "white" }}>Pricing</Link>
-      <Link href="/contact" style={{ textDecoration: "none", color: "white" }}>Contact</Link>
+      <Link href="/" passHref>
+        <span style={{ textDecoration: "none", color: "white", cursor: "pointer" }}>Home</span>
+      </Link>
+      <Link href="/about" passHref>
+        <span style={{ textDecoration: "none", color: "white", cursor: "pointer" }}>About</span>
+      </Link>
+      <Link href="/pricing" passHref>
+        <span style={{ textDecoration: "none", color: "white", cursor: "pointer" }}>Pricing</span>
+      </Link>
+      <Link href="/contact" passHref>
+        <span style={{ textDecoration: "none", color: "white", cursor: "pointer" }}>Contact</span>
+      </Link>
     </nav>
   );
 }
