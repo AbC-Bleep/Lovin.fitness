635b3dd651247a7c06d3d05e5c512c6d9bf5e902
AbC-Bleep <denilovin@gmail.com>
2025-02-24 12:09:35 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Navbar from "../components/Navbar"; // 
-import WorkoutPlans from "../components/WorkoutPlans";
+import Navbar from "../components/Navbar"; 
+import WorkoutPlans from "../components/WorkoutPlans"; 
 
 export default function Home() {
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
-      <Navbar /> {/* 
+      <Navbar />
       <h1>Welcome to Lovin Fitness</h1>
       <p>Building Stronger Women - Your personalized workout journey starts here!</p>
       <WorkoutPlans />
