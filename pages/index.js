5d3729fb5b5da4c5a573b4dd1d3e48bd584a7f82
AbC-Bleep <denilovin@gmail.com>
2025-02-24 12:06:51 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,13 @@
-import Head from 'next/head';
-import WorkoutPlans from '../components/WorkoutPlans';
+import Navbar from "../components/Navbar"; // 
+import WorkoutPlans from "../components/WorkoutPlans";
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Lovin Fitness</title>
-        <meta name="description" content="Personalized Workout Plans" />
-      </Head>
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <Navbar /> {/* 
       <h1>Welcome to Lovin Fitness</h1>
       <p>Building Stronger Women - Your personalized workout journey starts here!</p>
       <WorkoutPlans />
-    </div>
-import Navbar from "../components/Navbar";
-
-export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <h1>Welcome to Lovin Fitness</h1>
-      <p>Building Stronger Women - Your personalized workout journey starts here!</p>
     </div>
   );
 }
