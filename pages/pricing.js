c725f67d879a3980d402cc0118a53262b8101fde
AbC-Bleep <denilovin@gmail.com>
2025-02-24 11:56:44 -0700

    Create pricing.js

---
+++ pages/pricing.js
@@ -0,0 +1,12 @@
+export default function Pricing() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Pricing Plans</h1>
+      <p>We offer the best fitness plans for women!</p>
+      <ul style={{ listStyle: "none" }}>
+        <li>💪 Basic Plan - $9.99/month</li>
+        <li>🔥 Pro Plan - $19.99/month</li>
+      </ul>
+    </div>
+  );
+}
