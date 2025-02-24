23784816982f03f3ed13f88e67fc293bbe24450f
AbC-Bleep <denilovin@gmail.com>
2025-02-24 14:20:16 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,17 @@
-import Image from "next/image";
 import Navbar from "../components/Navbar";
+import WorkoutPlans from "../components/WorkoutPlans";
 
 export default function Home() {
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
-      {/* ✅ Display the image */}
-      <Image 
-        src="/lovin-fitness.jpg"  // The image is inside the /public folder
-        alt="Lovin Fitness Logo" 
-        width={500} 
-        height={200} 
-        style={{ maxWidth: "100%", height: "auto" }}
-      />
+      {/* Add the Image Here */}
+      <img src="/lovin-fitness.jpg" alt="Lovin Fitness Logo" 
+           style={{ width: "100%", maxWidth: "600px", margin: "20px auto", display: "block" }} />
 
-      {/* ✅ Add the Navbar */}
       <Navbar />
-
-      <h1>Welcome to Lovin Fitness 💖</h1>
-      <p>Building Stronger Women - Your personalized fitness journey starts here!</p>
+      <h1>Welcome to Lovin Fitness</h1>
+      <p>Building Stronger Women - Your personalized workout journey starts here!</p>
+      <WorkoutPlans />
     </div>
   );
 }
