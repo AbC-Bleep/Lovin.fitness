37db2b60adcd58ba18442622b701ea2f766f4ae9
AbC-Bleep <denilovin@gmail.com>
2025-02-24 11:56:04 -0700

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>About Lovin Fitness</h1>
+      <p>Lovin Fitness helps women of all ages achieve their fitness goals with personalized workout plans.</p>
+    </div>
+  );
+}
