a5b73f85dd152da87b6974d4ed55e17a0d05a16f
AbC-Bleep <denilovin@gmail.com>
2025-02-24 12:03:47 -0700

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,11 @@
+import Navbar from "../components/Navbar";
+
 export default function Contact() {
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
+      <Navbar />
       <h1>Contact Us</h1>
-      <p>Email: dlovinfitness@gmail.com
-      <p>Instagram: @lovinfitness</p>
+      <p>Have questions? Reach out to us at <strong>support@lovinfitness.com</strong></p>
     </div>
   );
 }
