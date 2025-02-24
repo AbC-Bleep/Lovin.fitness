0fcf461c15b2ab9965c8d0ab2b1f6e4aa5dccfb3
AbC-Bleep <denilovin@gmail.com>
2025-02-24 11:57:40 -0700

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Contact Us</h1>
+      <p>Email: dlovinfitness@gmail.com
+      <p>Instagram: @lovinfitness</p>
+    </div>
+  );
+}
