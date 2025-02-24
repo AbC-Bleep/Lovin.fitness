187eeed1823b112060f172cff2b4c3f8f5bf0ba2
AbC-Bleep <denilovin@gmail.com>
2025-02-24 12:00:34 -0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,8 @@
 export default function About() {
-  return (
+  return ( import Navbar from "../components/Navbar";
     <div style={{ textAlign: "center", padding: "20px" }}>
       <h1>About Lovin Fitness</h1>
       <p>Lovin Fitness helps women of all ages achieve their fitness goals with personalized workout plans.</p>
-import Navbar from "../components/Navbar";
    </div>
   );
 }
